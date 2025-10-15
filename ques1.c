ce1bd9f14ab2094b26890a0d5966ce69f25c34ff
SANYOM <sanyomthareja@gmail.com>
2025-10-15 22:51:04 +0530

    Day1

---
+++ ques1.c
@@ -0,0 +1,15 @@
+// Q1 (User Inputs, Operations & Output)
+// Write a program to input two numbers and display their sum.
+
+#include<stdio.h>
+int main()
+{
+    int n1;
+    int n2;
+    int sum;
+    scanf("%d",& n1);
+    scanf("%d",& n2);
+    sum=n1+n2;
+    printf("%d",sum);
+    return 0;
+}
